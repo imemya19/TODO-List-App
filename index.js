a5c8463a0f7705015aef3673af68f7a22aa75173
imemya19 <somyaagrawal.ime19@gmail.com>
2021-02-07 18:33:19 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();;
+const port = 8000;
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err) {
+
+    if (err) {
+        console.log(`Error in starting the server is : ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on port : ${port}`);
+});
